9625fe3cdb500fa3a36c5d9274566ec650b1ec8e
marwanahemm <marwan.ahemmane@gmail.com>
2026-06-14 14:56:08 +0200

    feat(seo): add redirects and create sitemap.xml for improved SEO

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  async redirects() {
+    return [
+      { source: '/1', destination: '/tutoriels/1', permanent: true },
+      { source: '/2', destination: '/tutoriels/2', permanent: true },
+      { source: '/3', destination: '/tutoriels/3', permanent: true },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
